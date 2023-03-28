ac22ac1e09f2bcb2f541274c4e60a95293422fcf
Marchenko Yaroslava <fortunefame1710@gmail.com>
2023-03-28 18:25:47 +0200

    Added template

---
+++ index.js
@@ -0,0 +1,15 @@
+//       Пути к обьектам HTML
+
+var task1 = document.getElementById('task1');
+var task2 = document.getElementById('task2');
+var task3 = document.getElementById('task3');
+var task4 = document.getElementById('task4');
+var task5 = document.getElementById('task5');
+var task6 = document.getElementById('task6');
+var task7 = document.getElementById('task7');
+var task8 = document.getElementById('task8');
+var task9 = document.getElementById('task9');
+var task10 = document.getElementById('task10');
+
+//
+
